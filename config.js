406d3122df038944513673094c04df2530bd116f
light lu <light.lu@advantech.com.tw>
2021-04-16 10:44:46 +0800

    mqtt websocket port=1884; url hostname for mqtt server host

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
-host = '172.22.24.183';	// hostname or IP address
-//host = '127.0.0.1';	// hostname or IP address
-// host = '172.16.153.110';	// hostname or IP address
-port = 9001;
+//host = '127.0.0.1';
+port = 1884;
 topic = '#';		// topic to subscribe to
 useTLS = false;
 username = null;
 password = null;
-// username = "jjolie";
-// password = "aa";
 
 // path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
 //    defaults to "/mqtt"
